60af1f61c5d0371f4c478ed2671360b05db2dbb5
Takwa AL MEKNI <takwa.al-mekni@proxym-it.com>
2021-12-03 05:01:01 +0100

    frontend folder

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="C:/Users/takwa.al-mekni/Desktop/Nuitd'Info/finaltest/socialite/resources/views/FrontEnd/index.html" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
                 </div>
